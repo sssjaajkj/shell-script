958efc30ca1ba92dc30cbac155fe77a3677d843a
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 16:34:46 +0530

    03-variable.shs

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,11 +1,11 @@
 
 #!/bin/bash
 
-Person1=Ramesh
-Person2=Suresh
+Person1=Ramesh v
+Person2=Suresh k
 
 
-echo "$Person1: Hello suresh , how r u?"
-echo "$Person2: Hello Ramesh ,i am fine, how r u?"
+echo "$Person1: Hello $Person2 , how r u?"
+echo "$Person2: Hello $Person1 ,i am fine, how r u?"
 echo "$Person1: Hi am fine , how  is your work?"
 echo "$Person2 : Hi am fine , how  is your work?"
