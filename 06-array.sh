3b48bd75a7d2a22ad7a252f1d8c0de053790c3c3
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 16:59:21 +0530

    06-arrays.shs

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bi/bash
+
+MOVIES=("RRR","DJTILL","MURARI")
+
+#list always start with 0 zer0
+
+echo "First Movie is : ${MOVIES[0]}"
+echo "second Movie is : ${MOVIES[1]}"
+
+echo "First Movie is : ${MOVIES[@]}" //All movies
+
