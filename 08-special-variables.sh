ae8fc721101ebb38733cfac7801c7e43f9c5d268
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 17:40:13 +0530

    data-type

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "All variables :$@"
-
 echo "Number of varible passed  :$#"
-
 echo "Script name   :$0"
+echo "Current working directory  :$PWD"
+echo "Home  directory  of current user :$HOME"
+echo "Which user is  runing this script :$USER"
+echo " Hostname :$HOSTNAME"
+echo "Proces ID of the current shell script : $$"
