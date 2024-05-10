d9f5da156a02542432a365bcc135eb6a5dbc4127
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 17:56:00 +0530

    data-type

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+Number1=$1
+
+
+if [ $Number1 -gt 10 ]
+{
+    echo "Given number is $Number1 is greater than 10"
+}
+fi{echo "Given number is $Number1 is less than 10"echo "Num2 is greater then num1"
+}
