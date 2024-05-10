0f8b8808e7c0484506d6688ab075789bf3a3308d
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 15:41:00 +0530

    03-gitcommand commited

---
+++ 03-gitcommand.sh
@@ -0,0 +1,17 @@
+# create github repo
+# clone the repo
+    *git clone <path>
+# develop code 
+#Add the files to temp area
+ git add <file-name>
+
+#commit the code
+ git commit -m "msg"
+
+ #push the code into local to central repo
+  git push -u origin main
+   
+
+   git add .; git commit -m "message"; git push - u origin main
+
+
