ddb062a9bd4780cf9dd04ac8857ff352ed672ea2
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 16:23:20 +0530

    02-variable.sh

---
+++ 02-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+each "Ramesh : Hello suresh , how r u?
+
+each "Suresh : Hello Ramesh ,i am fine, how r u?
+
+each "Ramesh : Hi am fine , how  is your work?
+
+each "Suresh : Hi am fine , how  is your work?
+
+
+#repeated word ramesh.suresh
+# if you wantt to change , you have to change it every where
+# While chaning accidently you may chnage actual code
+
+# Variables are solve
