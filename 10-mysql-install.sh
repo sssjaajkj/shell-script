915f1b485886db4cbd8ed938a741ac561fcef9c4
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 18:19:19 +0530

    data-type

--- 10-mysql-install.sh
+++ 10-mysql-install.sh
@@ -2,5 +2,17 @@
 
 
 #Instlling mysql s/w
+ 
+ #How do run command in shell script and takeing output#
+ USERID=$(id -u)
 
+ if [ $USERID -ne 0]
+ then 
+
+ echo "plz run this script with root access."
+
+
+ else 
+        echo "you are super user"
+fi
 dnf install mysql -y
