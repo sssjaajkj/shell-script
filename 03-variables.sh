a4164f0e4104c7a61cdd5f133c9e86266270343d
sssjaajkj <vshiva4646@gmail.com>
2024-05-10 16:37:42 +0530

    03-variable.shs

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,11 @@
 
 #!/bin/bash
 
+# // Declaring a variable
 Person1=Ramesh-v
 Person2=Suresh-k
 
-
+# // Refrerning a variable
 echo "$Person1: Hello $Person2 , how r u?"
 echo "$Person2: Hello $Person1 ,i am fine, how r u?"
 echo "$Person1: Hi am fine , how  is your work?"
