9aee394dafb9b92c85ee007cbaa51efc0c9e0d50
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 09:02:08 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -10,17 +10,14 @@
         {
         if [ $1 -ne 0]
             then
-            {
-                echo "$2 ... Failure..."
-                }
             
-            exit  1
+                echo "$2 ... Failure..."
+                 exit  1
           else
-          {
+          
             echo "$2 ... Pass..."
             
-           
-          }
+    
             fi   
             
             
