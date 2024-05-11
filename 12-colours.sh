9080abf30dd0d9830581fc181cfb2dc5700a99c9
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 12:02:27 +0530

    functions

--- 12-colours.sh
+++ 12-colours.sh
@@ -8,9 +8,9 @@
  TIMESTAMP=$(date +%F-%H-%M-%S)
  SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
  LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
- R=" \e[31m"
- G=" \e[32m"
- N=" \e[0m"
+ R="\e[31m"
+ G="\e[32m"
+ N="\e[0m"
 
         VALIDATE ()
         {
