19674100e46f0fda78c319c0abc24d6d1b0b6545
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 07:40:15 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+    #!/bin/bash
 
-USERID=(id -u)
+    USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-   
-    then 
-        
-        echo  "Please access with root user ...... FAILUAR..."
-        exit 1
-else
-   
-   echo "Is a root user....SUCESS..."
-   
-   fi
+    if [ $USERID -ne 0 ]
+    
+        then 
+            
+            echo  "Please access with root user ...... FAILUAR..."
+            exit 1
+    else
+    
+    echo "Is a root user....SUCESS..."
+    
+    fi
 
-   dnf install mysql -y
+    dnf install mysql -y
