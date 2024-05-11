09a1d981cb6358df9919688d0860db972b5b9b6c
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 08:26:46 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -15,16 +15,6 @@
         
     }
 
-
-   
-    if [ $USERID -ne 0 ]
-        then 
-            echo   "Please access with root user ...... FAILUAR..."
-            exit 1
-    else
-    
-    echo "Is a root user....SUCESS..."
-    
-    fi
+VALIDATE "$?" "Installation"
 
    
