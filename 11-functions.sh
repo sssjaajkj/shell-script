5558694f31ac9a8e372497d02f425e4092fb473e
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 08:34:14 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -8,7 +8,14 @@
 
         VALIDATE ()
         {
-            echo "hello "
+                if[ $1 -ne 0]
+                  then
+            echo "$2 ... Failure..."
+            exit  1
+          else
+              echo "$2 ... Pass..."
+              fi
+
         }
  if [ $USERID -ne 0 ] 
  then 
