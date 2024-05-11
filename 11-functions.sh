609dc1cb80ba3c0a85c9552751a2c4c5b657d8b7
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 08:29:21 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,18 +1,38 @@
-    #!/bin/bash
+#!/bin/bash
 
-    USERID=$(id -u)
-echo "HI"
-     VALIDATE(){
 
-        if [ $1 -ne 0]
-            then 
-             echo "$2 ... Installation Failed... "
+#Instlling mysql s/w
+ 
+ #How do run command in shell script and take output# in inter view
+ USERID=$(id -u)
 
-            exit 1
-            else 
-            echo "$2 ... Installation Pass ....
-        fi
-        
-    }
-   
-   echo "HI"
+ if [ $USERID -ne 0 ] 
+ then 
+    echo "plz run this script with root access."
+    exit 1 # manulaly exit if error comes 
+
+    else 
+            echo "you are super user."
+    fi
+        dnf install mysql -y
+        if [ $?-ne 0 ]
+        then 
+        echo "Installtion of mysql ... is FAILURE"
+
+        exit 1
+        else
+        echo  "installation mysql is success...."
+
+    fi
+
+    dnf install git -y
+
+    if [$? -ne 0]
+    then
+     echo " Installtion of git ... is FAILURE"
+    exit
+     else
+     echo "Installtion of git ... is SUCCESS"
+
+fi
+        echo " is script  proceeding ?"
