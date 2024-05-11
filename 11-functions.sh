9b09b16c60d3363df44c5b1eb8565179e29dd415
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 08:54:49 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -9,11 +9,11 @@
         VALIDATE ()
         {
         if [ $1 -ne 0]
-             then
+            then
             echo "$2 ... Failure..."
             exit  1
-          else
-              echo "$2 ... Pass..."
+          else{echo "$2 ... Pass..."}
+              
             
             fi
 
